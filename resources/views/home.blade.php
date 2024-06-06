9338791bdde940d48dd738b2de61192a779255ec
justinbondocponce <ponce.justin16@gmail.com>
2024-06-06 21:29:03 +0800

    hsgh

---
+++ resources/views/home.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h3 class="m-2">{{ __('COMPANY') }}</h3>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    
+    <section class="row">
+          <div class="col-lg-3 col-10">
+            <!-- small box -->
+            <div class="small-box bg-info">
+              <div class="inner]">
+                <h3>BSIT-3B EXAMINERS</h3>
+
+                <p></p>
+              </div>
+              <div class="icon">
+                <i class="ion ion-bag"></i>
+              </div>
+              <a href="#" class="small-box-footer"><a href="users">More info</a><i class="fas fa-arrow-circle-right"></i></a>
+              <button>submit</button>
+            </div>
+</section>
+    <!-- /.content -->
+@endsection
